08f062c4916ba0a1451d56baa76c938ab6fc6744
srisai-murari <bunnymurari1@gmail.com>
2025-09-05 19:28:09 +0530

    Create Task5c.java

---
+++ Task5/Task5c.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+class RemoveDuplicatesLinkedList {
+ static class Node {
+ int data;
+ Node next;
+ Node(int data) {
+ this.data = data;
+ this.next = null;
+ }}
+Node head;
+public void insert(int data) {
+ Node newNode = new Node(data);
+ if (head == null) {
+ head = newNode;
+ return; }
+ Node temp = head;
+ while (temp.next != null) {
+ temp = temp.next;
+ }
+ temp.next = newNode;}
+public void removeDuplicates() {
+ HashSet<Integer> seen = new HashSet<>();
+ Node current = head;
+ Node prev = null;
+while (current != null) {
+ if (seen.contains(current.data)) {
+ prev.next = current.next;
+ } else {
+ seen.add(current.data);
+ prev = current;
+ }
+ current = current.next;
+ }}
+public void display() {
+ Node temp = head;
+ while (temp != null) {
+ System.out.print(temp.data + " ");
+ temp = temp.next; }
+ System.out.println();}
+public static void main(String[] args) {
+ RemoveDuplicatesLinkedList list = new RemoveDuplicatesLinkedList();
+ list.insert(10);
+ list.insert(20);
+ list.insert(10);
+ list.insert(30);
+ list.insert(20);
+ System.out.println("Original List:");
+ list.display();
+ list.removeDuplicates();
+ System.out.println("After removing duplicates:");
+ list.display();
+ }}
